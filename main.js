70fc5d01870d1d878467c1d014372bf937709056
joaopedrosangaletti <107260154+joaopedrosangaletti@users.noreply.github.com>
2024-05-15 10:37:07 -0300

    DIA 15/05/24

--- main.js
+++ main.js
@@ -19,36 +19,33 @@ const tempoObjetivo2 = new Date("2024-06-05T00:00:00");
 const tempoObjetivo3 = new Date("2024-12-30T00:00:00");
 const tempoObjetivo4 = new Date("2025-01-05T00:00:00");
 
-const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
+const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4]
 
-function atualizaCronometro(){
+function atualizaCronometo(){
     for(let i= 0; i<contadores.length;i++){
-    contadores[i].textContent=calculaTempo(tempos[i]);
-    }
-}
-
-
 contadores[0].textContent = calculaTempo(tempoObjetivo1);
-function calculaTempo(tempoObjetivo){
-    let tempoAtual = new Date();
-    let tempoFinal = tempoObjetivo - tempoAtual;
-    let segundos = Math.floor(tempoFinal / 1000); 
-    let minutos = Math.floor(segundos / 60);
-    let horas = Math.floor(minutos / 60);
-    let dias = Math.floor(horas / 24);
-    segundos%= 60;
-    minutos%= 60;
-    horas%= 24;
-    if(tempoFinal>0){
-    return dias + "dias" + horas + "horas" + minutos + "minutos" +segundos + "segundos";
-    }
-    else{
-        return "Prazo Finalizado"
-    }
 }
-function comecaCronometro(){
-    atualizaCronometro();
-    setInterval(atualizaCronometro, 1000);
 }
 
-//comecaCronometro();
+function calculaTempo(tempoObjetivo){
+let tempoAtual = new Date();
+let tempoFinal = tempoObjetivo - tempoAtual;
+let segundos = Math.floor(tempoAtual / 1000);
+let minutos = Math.floor(segundos / 60);
+let horas = Math.floor(minutos /  60);
+let dias = Math.floor(horas / 24);
+segundos %= 60;
+minutos %= 60;
+horas %= 24;
+if(tempoFinal>0){
+return dias + "dias" + horas + "horas" + minutos +"minutos" + segundos + "segundos";
+}
+else{
+    return"prazo Finalizado"
+}
+}
+function comecaCrometro(){
+    atualizaCronometo();
+    setInterval(atualizaCronometo, 100)
+}
+//comaCronometro();
